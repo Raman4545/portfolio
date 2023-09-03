28ab01e45d7dcd3f3ab74922585b257836fbbde4
Ramanpreet Singh Pannu <ramanpreet.singh@perficient.com>
2023-09-03 23:57:13 +0530

    'fifth'

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
+const {check, validationResult } = require('express-validator');
 
 //@route  GET api/users
-//@desc   TEST route
+//@desc   Resgister user
 //@access Public
-router.get('/',(req,res)=>res.send('user route'));
+router.post('/',[
+    check('name','Name is Required').not().isEmpty(),
+    check('email','Enter valid email').isEmail(),
+    check('password','enter valid password').isLength({min:6})
+],(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    res.send('user route')
+}
+    );
+
+module.exports = router;
